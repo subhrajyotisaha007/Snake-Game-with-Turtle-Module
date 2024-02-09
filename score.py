de77be0ee29f5ee0eb22ad453748584c30de93a1
Subhrajyoti Saha <75173603+subhrajyotisaha007@users.noreply.github.com>
2024-02-09 08:37:57 +0530

    Update score.py

--- score.py
+++ score.py
@@ -4,7 +4,7 @@ FONT = ('Courier',24,'normal')
 class Score(Turtle):
     def __init__(self):
         super().__init__()
-        self.score = 4
+        self.score = 0
         self.color('white')
         self.hideturtle()
         self.penup()
